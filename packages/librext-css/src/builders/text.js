197c05fd5ea14baebecf31b9f9ed4aad03dd016c
RJ Salvador <salvador.workshop@gmail.com>
2025-11-08 09:38:23 -0500

    CSS gen - connecting new text data to builders

--- packages/librext-css/src/builders/text.js
+++ packages/librext-css/src/builders/text.js
@@ -17,67 +17,93 @@ const buildText = () => {
 
     const textDataFile = `${libRextCssUtil.dataDir}/text.json`
     const textData = libRextCssFileHandler.readFile(textDataFile)
-    console.log('[LibRext CSS - TextBuilder] textData', textData.typefaces)
+    // console.log('[LibRext CSS - TextBuilder] textData', textData)
 
     const styleVars = []
+
     const typefaces = []
-    const roles = []
     const typescale = []
+    const roles = []
+    const docroles = []
 
     for (const typefaceCtg in textData.typefaces) {
-        const dataTypefaceList = textData.typefaces[typefaceCtg]
-        const typefaceList = dataTypefaceList.map((typeName, idx) => {
-            return {
-                name: typeName,
-                category: typefaceCtg,
-                priority: idx + 1
-            }
-        })
-        typefaces.push(...typefaceList)
+        // const typefaceList = dataTypefaceList.map((typeName, idx) => {
+        //     return {
+        //         name: typeName,
+        //         category: typefaceCtg,
+        //         priority: idx + 1
+        //     }
+        // })
+        // typefaces.push(...typefaceList)
+
+        const currentTypefaces = textData.typefaces[typefaceCtg]
+        console.log('[Text Builder] typefaceCtg', typefaceCtg)
+        console.log('[Text Builder] currentTypefaces', currentTypefaces)
     }
     const getTypefaceByName = (name) => typefaces.find(tFace => tFace.name == name);
 
-    for (const typefaceRole in textData.roles) {
-        const dataTypefaceList = textData.roles[typefaceRole]
-        const typefaceList = dataTypefaceList.map((typeName, idx) => {
-            const typefaceData = getTypefaceByName(typeName)
-            return {
-                name: typeName,
-                category: typefaceData.category,
-                role: typefaceRole,
-                priority: idx + 1
-            }
-        })
-        roles.push(...typefaceList)
-    }
-    // console.log('[LibRext CSS - TextBuilder] typefaces', typefaces)
+    for (const tTypescale of textData.typescale) {
+        // const newProp = {
+        //     property: `typescale-${tScaleVal.style}`,
+        //     value: `${tScaleVal.value}rem`,
+        // }
+        // styleVars.push(newProp)
 
-    for (const tScaleVal of textData.typescale) {
-        const newProp = {
-            property: `typescale-${tScaleVal.style}`,
-            value: `${tScaleVal.value}rem`,
-        }
-        styleVars.push(newProp)
+        const currentTypescaleItem = tTypescale
+        console.log('[Text Builder] currentTypescaleItem', currentTypescaleItem)
     }
 
-    console.log('[LibRext CSS - TextBuilder] roles', roles)
-    roles.forEach(role => {
-        let genericVal = role.category
-        if (role.category == 'sans') {
-            genericVal = 'sans-serif'
-        } else if (role.category == 'display') {
-            genericVal = 'serif'
-        }
+    for (const tRoles in textData.roles) {
+        // const typefaceList = dataTypefaceList.map((typeName, idx) => {
+        //     const typefaceData = getTypefaceByName(typeName)
+        //     return {
+        //         name: typeName,
+        //         category: typefaceData.category,
+        //         role: typefaceRole,
+        //         priority: idx + 1
+        //     }
+        // })
+        // roles.push(...typefaceList)
 
-        const newProp = {
-            property: `fontfam-${role.role}-${role.priority}`,
-            value: `"${role.name}", ${genericVal}`,
-        }
-        styleVars.push(newProp)
-    })
+        const currentRole = textData.roles[tRoles]
+        console.log('[Text Builder] currentRole', currentRole)
+    }
 
-    const variablesContent = libRextCssUtil.writeCssVarRule(varsSelector, styleVars);
-    libRextCssFileHandler.writeFile(`${__dirname}/../../css/librext-text.css`, variablesContent)
+    for (const tDocroles in textData.docroles) {
+        // const dataTypefaceList = textData.roles[typefaceRole]
+        // const typefaceList = dataTypefaceList.map((typeName, idx) => {
+        //     const typefaceData = getTypefaceByName(typeName)
+        //     return {
+        //         name: typeName,
+        //         category: typefaceData.category,
+        //         role: typefaceRole,
+        //         priority: idx + 1
+        //     }
+        // })
+        // roles.push(...typefaceList)
+
+        const currentDocRole = textData.docroles[tDocroles]
+        console.log('[Text Builder] currentDocRole', currentDocRole)
+    }
+
+    // console.log('[LibRext CSS - TextBuilder] roles', roles)
+    // roles.forEach(role => {
+    //     let genericVal = role.category
+    //     if (role.category == 'sans') {
+    //         genericVal = 'sans-serif'
+    //     } else if (role.category == 'display') {
+    //         genericVal = 'serif'
+    //     }
+
+    //     const newProp = {
+    //         property: `fontfam-${role.role}-${role.priority}`,
+    //         value: `"${role.name}", ${genericVal}`,
+    //     }
+    //     styleVars.push(newProp)
+    // })
+
+    // const variablesContent = libRextCssUtil.writeCssVarRule(varsSelector, styleVars);
+    // libRextCssFileHandler.writeFile(`${__dirname}/../../css/librext-text.css`, variablesContent)
 }
 
 module.exports = { build: buildText };
