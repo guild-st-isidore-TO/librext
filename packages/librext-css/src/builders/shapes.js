7d44596bd189e37bb6b0eec5dfd069296ab742ab
RJ Salvador <salvador.workshop@gmail.com>
2025-11-11 09:22:23 -0500

    CSS gen - variables generated for shapes

--- packages/librext-css/src/builders/shapes.js
+++ packages/librext-css/src/builders/shapes.js
@@ -18,26 +18,34 @@ const buildShapes = () => {
     const shapeDataFile = `${libRextCssUtil.dataDir}/shapes.json`
     const shapeData = libRextCssFileHandler.readFile(shapeDataFile)
     // console.log('[LibRext CSS - ShapesBuilder] shapeData', shapeData)
-    
+
     const styleVars = []
     
+    for (const [key, value] of Object.entries(shapeData.librextScale)) {
+        const scaleVar = {
+            property: `librext-scale-${key.replace('ls', '')}`,
+            value: `${value}px`,
+        }
+        styleVars.push(scaleVar);
+    }
+
     const cRadiusScale = shapeData.cornerRadiusScale
     for (const [key, value] of Object.entries(cRadiusScale)) {
         const cRadiusEntry = {
-            property: `corner-radius-${key}`,
-            value: `${value}px`,
+            property: `cor-radius-${key}`,
+            value: `var(--librext-scale-${value.replace('ls', '')})`,
         }
         styleVars.push(cRadiusEntry);
     }
-    
+
     for (const [key, value] of Object.entries(shapeData.elementHeightScale)) {
         const heightEntry = {
-            property: `element-height-${key}`,
-            value: `${value}px`,
+            property: `height-${key}`,
+            value: `var(--librext-scale-${value.replace('ls', '')})`,
         }
         styleVars.push(heightEntry);
     }
-    
+
     // console.log('[LibRext CSS - ShapesBuilder] styleVars', styleVars)
 
     const variablesContent = libRextCssUtil.writeCssVarRule(varsSelector, styleVars);
