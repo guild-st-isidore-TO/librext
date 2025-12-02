d4ca01a4ce826e35ef7d0d920b5b1076beb357ff
RJ Salvador <salvador.workshop@gmail.com>
2025-12-02 14:48:57 -0500

    CSS gen -- colours are now dynamically generated

--- packages/librext-css/src/builders/html/build-html-colours.js
+++ packages/librext-css/src/builders/html/build-html-colours.js
@@ -8,107 +8,57 @@ const buildHtmlColours = () => {
   const template = libRextCssFileHandler.readTemplateFile(fPath);
   // console.log('[LibRext CSS - Build HTML Colours] template', template);
 
+  const coloursDataFile = `${libRextCssUtil.dataDir}/colours.json`
+  const coloursData = libRextCssFileHandler.readJsonFile(coloursDataFile)
+
+  const dataThemeLight = []
+  const dataPaletteItems = []
+  for (const themeColName in coloursData.variables.palette.theme) {
+    currentThemeCol = coloursData.variables.palette.theme[themeColName]
+    dataThemeLight.push({
+      colour: currentThemeCol,
+    })
+    dataPaletteItems.push({
+      colour: currentThemeCol,
+      description: 'Sed pretium orci eros, quis feugiat'
+    })
+  }
+
+  const dataThemeDark = dataThemeLight
+
+  const dataGreyscaleLight = coloursData.variables.palette.grey.map(greyCol => {
+    return {
+      colour: greyCol,
+    }
+  })
+
+  const dataGreyscaleDark = dataGreyscaleLight
+
+
+  const dataCollayersLight = [
+    dataGreyscaleLight[2],
+    dataGreyscaleLight[1],
+    dataGreyscaleLight[2],
+    dataGreyscaleLight[1],
+  ]
+
+  const dataColLayersDark = [
+    dataGreyscaleLight[10],
+    dataGreyscaleLight[9],
+    dataGreyscaleLight[10],
+    dataGreyscaleLight[9],
+  ]
+
   const templatePayload = {
-    themePaletteLight: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-    ],
-    themePaletteDark: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-            {
-        colour: '#02a70c'
-      },
-    ],
-    greyscalePaletteLight: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-            {
-        colour: '#02a70c'
-      },
-    ],
-    greyscalePaletteDark: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-            {
-        colour: '#02a70c'
-      },
-    ],
-    paletteDetailItems: [
-      {
-        colour: '#1E90FF',
-        description: 'Sed pretium orci eros, quis feugiat'
-      },
-      {
-        colour: '#EDFF0A',
-        description: 'Sed pretium orci eros, quis feugiat'
-      },
-      {
-        colour: '#EC00FF',
-        description: 'Sed pretium orci eros, quis feugiat'
-      },
-      {
-        colour: '#02a70c',
-        description: 'Sed pretium orci eros, quis feugiat'
-      },
-    ],
-    colourLayersLight: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-      {
-        colour: '#02a70c'
-      },
-    ],
-    colourLayersDark: [
-      {
-        colour: '#1E90FF'
-      },
-      {
-        colour: '#EDFF0A'
-      },
-      {
-        colour: '#EC00FF'
-      },
-      {
-        colour: '#02a70c'
-      },
-    ],
+    themePaletteLight: dataThemeLight,
+    themePaletteDark: dataThemeDark,
+    greyscalePaletteLight: dataGreyscaleLight,
+    greyscalePaletteDark: dataGreyscaleDark,
+    paletteDetailItems: dataPaletteItems,
+    colourLayersLight: dataCollayersLight,
+    colourLayersDark: dataColLayersDark,
   };
+  console.log(templatePayload);
 
   const output = ejs.render(template, templatePayload);
 
