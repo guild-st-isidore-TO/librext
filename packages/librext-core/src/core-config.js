6ec3d3657afdd5407be3e3ebc7c2e836c2e0fc0f
RJ Salvador <salvador.workshop@gmail.com>
2025-12-09 17:07:35 -0500

    Created new config file

---
+++ packages/librext-core/src/core-config.js
@@ -0,0 +1,63 @@
+// DEFAULT VALUES
+const DEF_TOKEN_PREFIX = 'lbrxt'
+const DEF_FILENAME_PREFIX = 'librext'
+const DEF_OUT_DIRECTORY = '/dist' // relative to project root
+const DEF_ASSET_OUT_DIRECTORY = '/dist' // relative to project root
+const DEF_FORMAT = 'screen'
+const DEF_LINK_COLOUR = 'primary'
+const DEF_LINK_COLOUR_HOVER = 'tertiary'
+const DEF_LINK_COLOUR_ACTIVE = 'tertiary'
+const DEF_LINK_COLOUR_VISITED = 'secondary'
+
+const tokenPrefix = DEF_TOKEN_PREFIX;
+const filenamePrefix = DEF_FILENAME_PREFIX;
+const outDirectory = DEF_OUT_DIRECTORY;
+const assetOutDirectory = DEF_ASSET_OUT_DIRECTORY;
+const format = DEF_FORMAT;
+const linkColour = DEF_LINK_COLOUR;
+const linkColourHover = DEF_LINK_COLOUR_HOVER;
+const linkColourActive = DEF_LINK_COLOUR_ACTIVE;
+const linkColourVisited = DEF_LINK_COLOUR_VISITED;
+
+const coreConfig = {
+    DEF_TOKEN_PREFIX,
+    DEF_FILENAME_PREFIX,
+    DEF_OUT_DIRECTORY,
+    DEF_ASSET_OUT_DIRECTORY,
+    DEF_FORMAT,
+    DEF_LINK_COLOUR,
+    DEF_LINK_COLOUR_HOVER,
+    DEF_LINK_COLOUR_ACTIVE,
+    DEF_LINK_COLOUR_VISITED,
+    tokenPrefix,
+    filenamePrefix,
+    outDirectory,
+    assetOutDirectory,
+    format, // (screen/print/ebook), 
+    linkColour, // (which palette colour to use for HTML links), 
+    linkColourHover,
+    linkColourActive,
+    linkColourVisited,
+}
+
+export default coreConfig
+export {
+    DEF_TOKEN_PREFIX,
+    DEF_FILENAME_PREFIX,
+    DEF_OUT_DIRECTORY,
+    DEF_ASSET_OUT_DIRECTORY,
+    DEF_FORMAT,
+    DEF_LINK_COLOUR,
+    DEF_LINK_COLOUR_HOVER,
+    DEF_LINK_COLOUR_ACTIVE,
+    DEF_LINK_COLOUR_VISITED,
+    tokenPrefix,
+    filenamePrefix,
+    outDirectory,
+    assetOutDirectory,
+    format,
+    linkColour,
+    linkColourHover,
+    linkColourActive,
+    linkColourVisited,
+}
--- packages/librext-core/src/index.js
+++ packages/librext-core/src/index.js
@@ -1,12 +1,14 @@
 "use strict"
 
+import coreConfig from "./core-config"
+
 /**
  * Custom Librext UI Data (doesn't conform to Theme-UI spec)
  * @namespace libRextCore
  */
 const libRextCore = () => {
     const outCore = {
-        config: {},
+        config: coreConfig,
     }
     return outCore
 }
