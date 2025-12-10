28478a9a49be3bb34f9163c5ff3dfd83a5c18be5
RJ Salvador <salvador.workshop@gmail.com>
2025-12-09 21:08:42 -0500

    Refactored CSS builder

--- packages/librext-css-builder/src/builders/colours.js
+++ packages/librext-css-builder/src/builders/colours.js
@@ -1,7 +1,7 @@
 "use strict"
 
-import libRextCssFileHandler from './file-handler.js'
-import libRextCssUtil from './utils.js'
+import fileHandler from '../file-handler.js'
+import cssHandler from '../css-handler.js'
 
 let scope = 'global'
 const defaultLocalScope = '.librext *'
@@ -38,8 +38,8 @@ const buildColours = (uiSpec, outputDir, config) => {
                 property: `background-color`,
                 value: colValue,
             }]
-            colClassContent += libRextCssUtil.writeCssRule(`.${config.tokenPrefix}-col-${colName}`, colClassProps)
-            bgColClassContent += libRextCssUtil.writeCssRule(`.${config.tokenPrefix}-bgcol-${colName}`, bgColClassProps)
+            colClassContent += cssHandler.writeCssRule(`.${config.tokenPrefix}-col-${colName}`, colClassProps)
+            bgColClassContent += cssHandler.writeCssRule(`.${config.tokenPrefix}-bgcol-${colName}`, bgColClassProps)
         } else {
             for (const [darkColName, darkColValue] of Object.entries(uiSpec.colors.modes.dark)) {
                 varProps.push({
@@ -54,13 +54,13 @@ const buildColours = (uiSpec, outputDir, config) => {
                     property: `background-color`,
                     value: darkColValue,
                 }]
-                colDarkClassContent += libRextCssUtil.writeCssRule(`.${config.tokenPrefix}-col-dark-${darkColName}`, colDarkClassProps)
-                bgColDarkClassContent += libRextCssUtil.writeCssRule(`.${config.tokenPrefix}-bgcol-dark-${darkColName}`, bgColDarkClassProps)
+                colDarkClassContent += cssHandler.writeCssRule(`.${config.tokenPrefix}-col-dark-${darkColName}`, colDarkClassProps)
+                bgColDarkClassContent += cssHandler.writeCssRule(`.${config.tokenPrefix}-bgcol-dark-${darkColName}`, bgColDarkClassProps)
             }
         }
     }
 
-    let variablesContent = libRextCssUtil.writeCssVarRule(varsSelector, varProps);
+    let variablesContent = cssHandler.writeCssVarRule(varsSelector, varProps);
     variablesContent += '\n'
     colClassContent += '\n'
     bgColClassContent += '\n'
@@ -70,40 +70,40 @@ const buildColours = (uiSpec, outputDir, config) => {
     const baseRules = [
         { property: 'background-color', value: `var(--${config.tokenPrefix}-col-background)` }
     ]
-    let baseContent = libRextCssUtil.writeCssRule('body', baseRules)
+    let baseContent = cssHandler.writeCssRule('body', baseRules)
 
     const linkRuleCol = uiSpec.colors[config.linkColour]
     const linkRules = [
         { property: 'color', value: linkRuleCol },
         { property: 'text-decoration-color', value: linkRuleCol }
     ]
-    linkColoursContent += libRextCssUtil.writeCssRule(`a:link, .${config.tokenPrefix}-link:link`, linkRules) + '\n'
+    linkColoursContent += cssHandler.writeCssRule(`a:link, .${config.tokenPrefix}-link:link`, linkRules) + '\n'
 
     const visitedRuleCol = uiSpec.colors[config.linkColourVisited]
     const visitedRules = [
         { property: 'color', value: visitedRuleCol },
         { property: 'text-decoration-color', value: visitedRuleCol }
     ]
-    linkColoursContent += libRextCssUtil.writeCssRule(`a:visited, .${config.tokenPrefix}-link:visited`, visitedRules) + '\n'
+    linkColoursContent += cssHandler.writeCssRule(`a:visited, .${config.tokenPrefix}-link:visited`, visitedRules) + '\n'
 
     const hoverRuleCol = uiSpec.colors[config.linkColourHover]
     const hoverRules = [
         { property: 'color', value: hoverRuleCol },
         { property: 'text-decoration-color', value: hoverRuleCol }
     ]
-    linkColoursContent += libRextCssUtil.writeCssRule(`a:hover, .${config.tokenPrefix}-link:hover`, hoverRules) + '\n'
+    linkColoursContent += cssHandler.writeCssRule(`a:hover, .${config.tokenPrefix}-link:hover`, hoverRules) + '\n'
 
     const activeRuleCol = uiSpec.colors[config.linkColourActive]
     const activeRules = [
         { property: 'color', value: activeRuleCol },
         { property: 'text-decoration-color', value: activeRuleCol }
     ]
-    linkColoursContent += libRextCssUtil.writeCssRule(`a:active, .${config.tokenPrefix}-link:active`, activeRules) + '\n'
+    linkColoursContent += cssHandler.writeCssRule(`a:active, .${config.tokenPrefix}-link:active`, activeRules) + '\n'
 
 
     const allContent = variablesContent + baseContent + colClassContent + bgColClassContent + colDarkClassContent + bgColDarkClassContent + linkColoursContent
 
-    libRextCssFileHandler.writeFile(`${outputDir}/css/${config.filenamePrefix}-colours.css`, allContent)
+    fileHandler.writeFile(`${outputDir}/css/${config.filenamePrefix}-colours.css`, allContent)
 }
 
 // module.exports = { build: buildColours };
