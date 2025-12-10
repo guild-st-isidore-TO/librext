eada5c6c180f32e101bd6c2265cae6800c206a5a
RJ Salvador <salvador.workshop@gmail.com>
2025-12-10 09:18:24 -0500

    Cleanup/refactoring around building docs and building website

--- src/build-styledoc.js
+++ src/build-styledoc.js
@@ -8,7 +8,7 @@ import { config as coreConfig } from 'librext-core'
 import libRextUiSpec from 'librext-ui-spec'
 import librextSpecBuilder from 'librext-spec-builder'
 import libRextCssBuilder from 'librext-css-builder'
-import libRextSiteBuilder from 'librext-site'
+import libRextDocsBuilder from 'librext-docs-builder'
 
 import { projectRootDir } from './builder-util.js'
 
@@ -16,12 +16,12 @@ const config = coreConfig
 // const outDir = coreConfig.outDirectory
 // const assetOutDir = coreConfig.assetOutDirectory
 
-const cssOutDir = join(projectRootDir, 'docs')
-const htmlOutDir = join(projectRootDir, 'docs')
+const cssOutDir = join(projectRootDir, 'docs/style')
+const htmlOutDir = join(projectRootDir, 'docs/style')
 const uiSpec = librextSpecBuilder(libRextUiSpec, config)
 
-console.log('[LibRext Documentation/Specs]\nCSS output directory', cssOutDir)
+console.log('[LibRext Style Documentation]\nCSS output directory', cssOutDir)
 libRextCssBuilder(uiSpec, cssOutDir, config)
 
-console.log('[LibRext Documentation/Specs]\nHTML output directory', htmlOutDir)
-libRextSiteBuilder(uiSpec, htmlOutDir, config)
+console.log('[LibRext Style Documentation]\nHTML output directory', htmlOutDir)
+libRextDocsBuilder(uiSpec, htmlOutDir, config)
