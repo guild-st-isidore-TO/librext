13712da42945cbf81107b17ba88ae29f1ff39255
RJ Salvador <salvador.workshop@gmail.com>
2025-12-10 19:27:21 -0500

    WIP - settled on a configuration usage/interface

--- src/build-styledoc.js
+++ src/build-styledoc.js
@@ -39,36 +39,67 @@ let specOverrides = {
 if (styleVariant == 'sw') {
     console.log('[LibRext Docs Builder] using SW styles')
 
-    specOverrides.colors.modes.dark.background = '#030D4F'
+    configOverrides = {
+        fonts: {
+            body: 'open-sans',
+            bodyAlt: 'crimson-text',
+            heading: 'aleo',
+            headingAlt: 'yrsa',
+            display: 'yrsa',
+        }
+    }
 
-    specOverrides.colors.primary = '#EE4603'
-    specOverrides.colors.secondary = '#061999'
-    specOverrides.colors.tertiary = '#FC8352'
-
-    specOverrides.colors.modes.dark.primary = '#FDC95C'
-    specOverrides.colors.modes.dark.secondary = '#FC8352'
-    specOverrides.colors.modes.dark.tertiary = '#061999'
+    specOverrides = {
+        primary: '#EE4603',
+        secondary: '#061999',
+        tertiary: '#FC8352',
+        colors: {
+            modes: {
+                dark: {
+                    background: '#030D4F',
+                    primary: '#FDC95C',
+                    secondary: '#FC8352',
+                    tertiary: '#061999',
+                }
+            }
+        }
+    }
 } else if (styleVariant == 'gsi') {
     console.log('[LibRext Docs Builder] using GSI styles')
 
-    specOverrides.colors.primary = '#B81626'
-    specOverrides.colors.secondary = '#FCDD09'
-    specOverrides.colors.tertiary = '#322928'
-
-    specOverrides.colors.modes.dark.primary = '#B81626'
-    specOverrides.colors.modes.dark.secondary = '#FCDD09'
-    specOverrides.colors.modes.dark.tertiary = '#322928'
+    specOverrides = {
+        primary: '#B81626',
+        secondary: '#FCDD09',
+        tertiary: '#322928',
+        colors: {
+            modes: {
+                dark: {
+                    primary: '#B81626',
+                    secondary: '#FCDD09',
+                    tertiary: '#322928',
+                }
+            }
+        }
+    }
 
 } else if (styleVariant == 'inc') {
     console.log('[LibRext Docs Builder] using INC styles')
 
-    specOverrides.colors.primary = '#B81626'
-    specOverrides.colors.secondary = '#FCDD09'
-    specOverrides.colors.tertiary = '#322928'
-
-    specOverrides.colors.modes.dark.primary = '#B81626'
-    specOverrides.colors.modes.dark.secondary = '#FCDD09'
-    specOverrides.colors.modes.dark.tertiary = '#322928'
+    specOverrides = {
+        primary: '#EE4603',
+        secondary: '#061999',
+        tertiary: '#FC8352',
+        colors: {
+            modes: {
+                dark: {
+                    background: '#030D4F',
+                    primary: '#FDC95C',
+                    secondary: '#FC8352',
+                    tertiary: '#061999',
+                }
+            }
+        }
+    }
 } else {
     console.log('[LibRext Docs Builder] using default styles')
 }
